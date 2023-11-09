d356084cfbe4feb48102078f502f2a882a01fba0
Suto <sutou1335michimasa@gmail.com>
2023-11-09 16:34:51 +0900

    test  auto generate storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/app/components/**/*.story.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -15,5 +15,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ['../public'],
 };
 export default config;
